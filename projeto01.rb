d13727b8b093209ef701c990ff84b566231bf909
unknown <larissa8oliveira@gmail.com>
2022-11-12 21:11:08 -0300

    Versão 2.0 Incluindo função no projeto

--- projeto01.rb
+++ projeto01.rb
@@ -1,58 +1,56 @@
 # Inicio
-puts '.:Olá Seja Bem-Vindo:.'
-# Menu Inicial
-puts 'Selecione a opção desejada:'
-puts ' [1] COMPRAR '
-puts ' [2] SAIR '
-# Opção
-subtotal=0.00
-opcao = gets.chomp.to_i
-while opcao == 1
-  puts "A opção  selecionada foi COMPRAR!"
-  puts "Conheça nossos produtos!"
-  puts"-----------------------"
-  puts "[1]Ração:R$257,25"
-  puts "[2]Brinquedos:R$60,25"
-  puts "[3]Acessório:R$34,25"
-  puts"-----------------------"
-   #Seleção de Produto 
-  puts " Qual o produto desejado?"
-  produto=gets.chomp.to_i
-  valor=0.00
-   #Condição para atribuição do valor do produto   selecionado
-    case produto 
-      when 1
-        puts "Ração"
-        valor= 257.25
-      when 2
-        puts " Brinquedos"
-        valor= 60.25
-      when 3
-        puts "Acessório"
-        valor= 34.25
-    end
-        #Informar a quntidade desejada do produto 
-        #selecionado
-        puts "Qual a quantidade desejada?"
-        quantidade=gets.chomp.to_i
-        #Calcular o subtotal do produto informado   
-         #mutiplicando o valor pela quntidade
-        subtotal=subtotal+(valor*quantidade) 
-        puts "Subtotal:#{subtotal}"
-        menu=1
-        puts "Digite 0 para voltar ao menu inicial!"
-        menu  = gets.chomp.to_i
-        puts "O Valor informado foi #{menu}"
-        
-        if menu == 0 
-          puts 'Selecione a opção desejada:'
-          puts ' [1] COMPRAR '
-          puts ' [2] SAIR '
-          opcao = gets.chomp.to_i
-          # Opção
-          # Retornar ao menu inicial caso a quantidade 
-        end      
-end 
- puts "A opção  selecionada foi SAIR!"
- puts ".:Obrigada pela visita até breve!:."
+puts".:HELLO WELCOME TO HAPPY DOG – PET SHOP:."
+#Frases que serão exibidas para o usuário 
 
+menus= {"start_menu"=>["Selecione a opção desejada:", "[1] COMPRAR ", "[2] SAIR"],
+  "product_menu"=>["Conheça nossos produtos!","[1]Ração:R$257,50","[2]Brinquedos:R$60,50","[3]Acessório:R$34,25", "Informe qual o produto desejado:"]
+}
+frases= {"quantidade"=>["Informe a quntidade desejada:"],
+  "start_menu1"=>["Informe 0 para retoenar ao menu inicial"]
+}
+# Váriavel que armazenará o valor do subtotal sendo inicializada com 0.00
+subtotal=0.0
+
+#Array hash Contendo os produtos e os repectivos valores
+product={"racao" => 257.50 , "brinquedos" =>60.50, "acessorio"=>34.25}
+
+#Função sair
+
+def sair
+  puts "A opção  selecionada foi SAIR!"
+  puts ".:Obrigada pela visita até breve!:."
+end 
+
+#Inicio da execução do programa 
+puts menus["start_menu"]
+option=gets.chomp.to_i
+
+# Estrutura de repetição caso a opção selecionada for comprar
+
+    while option == 1
+      puts menus["product_menu"]
+      option=gets.chomp.to_i
+          case option 
+            when 1 
+              puts frases["quantidade"]
+              quantidade=gets.chomp.to_i
+              subtotal+=product["racao"]*quantidade
+            when 2
+              puts frases["quantidade"]
+              quantidade=gets.chomp.to_i
+              subtotal+= product["brinquedos"]*quantidade
+            when 3
+              puts frases["quantidade"]
+              quantidade=gets.chomp.to_i
+              subtotal+= product["acessorio"]*quantidade
+          end
+          puts "Subtotal:#{subtotal}"
+          puts frases["start_menu1"]
+          menu=1
+          menu=gets.chomp.to_i
+              if menu==0
+                puts menus["start_menu"]
+                option=gets.chomp.to_i
+              end
+    end 
+    puts sair
